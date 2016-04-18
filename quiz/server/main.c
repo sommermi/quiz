302d0b00098ee0a4551d7e2090538b59404f50b7
sommermi <sommermi@hs-weingarten.de>
2016-04-18 11:40:56 +0200

    Singleton Pattern umgesetzt via Lock file. STG+c führt noch zu Problemen: Lösungsansatz Signalhandler implemntieren.

--- quiz/server/main.c
+++ quiz/server/main.c
@@ -8,15 +8,80 @@
  */
 
 #include "common/util.h"
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
+#include "main.h"
 
 int main(int argc, char **argv)
 {
-	setProgName(argv[0]);
-	/* debugEnable() */
+	switch(mainCreateLockfile())
+	{
+		case 0:	 perror("Error: Another instance of the server is already running!");
+				 return -1;
+		case 1:  printf("\nStarting server...");
+				 break;
+		case -1: perror("Error: Problems while creating lock file");
+				 return -1;
+	}
 
-	infoPrint("Server Gruppe xy");
-	printf("\nDas ist ein Test");		//Test commit
+
+	if(mainDeleteLockfile() != 0)
+	{
+		perror("Error: Deleting lockfile failed!");
+	}
 
 	return 0;
+}
+
+/**
+ * int mainCreateLockfile(void)
+ *
+ * This function creates a lock file to ensure that only one instance
+ * of the server is running on the machine.
+ * The default path to the lockfile is /tmp/programRunning. This path
+ * is specified in main.h.
+ *
+ * Return values:
+ * 0 indicates that there is already one instance running
+ * 1 indicates that no instnace is running => file was sucessfully created
+ * -1 indicates that there was an eeror creating the lock file (lack of memory etc.)
+ */
+int mainCreateLockfile(void)
+{
+	int fd = open(lockFilePath, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if(fd < 0)
+	{
+		if(errno == EEXIST)		//file already exists => Instance of server already running
+		{
+			return 0;
+		}
+		else	//problems with open()
+		{
+			return -1;
+		}
+	}
+	else	//file was created sucessfully => no instance was running
+	{
+		close(fd);
+		return 1;
+	}
+}
+
+/**
+ * int mainDeleteLockfie(void)
+ *
+ * This function deletes the crated lock file that ensures that only one instance
+ * of the server is running.
+ * The function is called at the end of a game as part of the cleanup routine to
+ * free all resources that were occupied by the server process.
+ *
+ * Return values:
+ * !=0 indicates that there an error has occured while deleting the lock file
+ */
+int mainDeleteLockfile(void)
+{
+	return unlink(lockFilePath);
 }
---
+++ quiz/server/main.h
@@ -0,0 +1,18 @@
+/*
+ * main.h
+ *
+ *  Created on: 18.04.2016
+ *      Author: sommer
+ */
+
+#ifndef SERVER_MAIN_H_
+#define SERVER_MAIN_H_
+
+
+static char lockFilePath[] = "/tmp/programRunning";
+
+int mainCreateLockfile(void);
+int mainDeleteLockfile(void);
+
+
+#endif /* SERVER_MAIN_H_ */
